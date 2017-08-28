cdf68fd65241300e77bb0731b45de37df1921245
Niek Blankers <niek@niekproductions.com>
2017-08-28 09:52:12 +0200

    Skeleton code, print P1 telegrams to serial

---
+++ p1meter.ino
@@ -0,0 +1,90 @@
+#include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+const char* ssid = "NeFas";
+const char* password = "";
+const char* hostName = "p1meter";
+
+
+
+#define MAXLINELENGTH 64 // longest normal line is 47 char (+3 for \r\n\0)
+char telegram[MAXLINELENGTH];
+
+#define SERIAL_RX     4  // pin for SoftwareSerial RX
+SoftwareSerial mySerial(SERIAL_RX, -1, false, MAXLINELENGTH); // (RX, TX. inverted, buffer)
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+  mySerial.begin(115200);
+
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname(hostName);
+
+  // No authentication by default
+  // ArduinoOTA.setPassword((const char *)"123");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+
+
+void readTelegram() {
+  if (mySerial.available()) {
+
+    while (mySerial.available()) {
+      int len = mySerial.readBytesUntil('\n', telegram, MAXLINELENGTH);
+      telegram[len] = '\n';
+      telegram[len+1] = 0;
+      yield();
+      Serial.print(telegram);
+    } 
+  }
+}
+
+
+
+void loop() {
+  readTelegram();
+  ArduinoOTA.handle();
+}
+
+
+
